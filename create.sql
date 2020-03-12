5a83be9f3785686a4a689f02cfa94bffba009eaf
drishan <43171376+drish96@users.noreply.github.com>
2020-03-12 14:37:19 -0600

    Add files via upload

---
+++ create.sql
@@ -0,0 +1,96 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+CREATE TYPE genre AS ENUM (
+    'Electronic',
+    'Rock',
+    'Hip-Hop',
+    'Rap',
+    'Folk',
+    'Country',
+    'Jazz',
+    'Blues',
+    'R&B',
+    'Pop',
+    'Classical',
+    'Funk',
+    'Reggae',
+    'Techno',
+    'House',
+    'Indie'
+);
+
+CREATE TABLE user (
+    user_id SERIAL PRIMARY KEY,
+    spotify_token 
+    username text NOT NULL,
+    password text NOT NULL,
+    first_name text NOT NULL,
+    last_name text NOT NULL,
+    email text,
+    fav_genre genre,
+    friendlist_id smallint NOT NULL,
+    activebool boolean DEFAULT true NOT NULL,
+    create_date date DEFAULT ('now'::text)::date NOT NULL,
+    last_update timestamp with time zone DEFAULT now(),
+    active integer
+);
+
+CREATE TABLE photo (
+    photo_id SERIAL PRIMARY KEY,
+    name text NOT NULL,
+    href text,
+    last_update timestamp with time zone DEFAULT now(),
+);
+
+CREATE TABLE video (
+    video_id SERIAL PRIMARY KEY,
+    name text NOT NULL,
+    href text,
+    duration_ms integer,
+    last_update timestamp with time zone DEFAULT now(),
+);
+
+CREATE TABLE playlist (
+    playlist_id SERIAL PRIMARY KEY,
+    user_id int,
+    track_ids --,
+    tracks.data --,
+    genre text,
+    duration_ms integer,
+    last_update timestamp with time zone DEFAULT now(),
+);
+
+CREATE TABLE track (
+    track_id SERIAL PRIMARY KEY,
+    title text NOT NULL,
+    artist text NOT NULL,
+    genre text,
+    spotify_url text,
+    duration_ms integer,
+    last_update timestamp with time zone DEFAULT now(),
+);
+
+CREATE TABLE notification (
+    notif_id SERIAL PRIMARY KEY,
+    user_id int,
+    sender text NOT NULL,
+    type int,
+    sent timestamp with time zone,
+);
+
+CREATE TABLE artist (
+    artist_id SERIAL PRIMARY KEY,
+    artist_name text NOT NULL,
+    music_type genre,
+    activebool boolean DEFAULT true NOT NULL,
+    create_date date DEFAULT ('now'::text)::date NOT NULL,
+    last_update timestamp with time zone DEFAULT now(),
+    active integer
+);
