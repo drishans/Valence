60f6797d064d43f40aab3445c421a573b8c37a83
Yosan Russom <59852875+yoru8317@users.noreply.github.com>
2020-04-20 13:33:32 -0600

    Delete search_box.jsx

--- src/components/search_box/search_box.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux
-
-import withUiActions from '../../hoc/uiHoc';
-import { fetchSearchData } from '../../store/actions/searchActions';
-
-
-import './Search_box.css';
