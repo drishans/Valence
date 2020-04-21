bb42d4f79b90b3e806f4e21f384c3b4fc9b3cb82
Yosan Russom <59852875+yoru8317@users.noreply.github.com>
2020-04-20 22:17:12 -0600

    Updated search feature

--- search.js
+++ search.js
@@ -1,37 +1,117 @@
-const express = require("express");
-const https = require("https");
-const bodyParser = require("body-parser");
+(function() {
+    // Declare Variables
+    var glitchUrl = "https://elegant-croissant.glitch.me/spotify"; //This url will make a request to the Spotify search API 
+    var spotifyUrl = "";
+    var users_input = "";
+    var categoryList = "";
+    var search_results = "";
+    var music_info = "";
+    var infiniteScroll = "?scroll=infinite"; // Users expect to scroll
 
-var SpotifyWebApi = require('spotify-web-api-node');
+    //The search button 
+    $(".search").on("click", function() {
+        getNewResults();
+    });
+    //Users input
+    $("input").on("keydown", function(event) {
+        if (event.which === 13) {// Enter a key
+            getNewResults();//search for results
+        }
+    });
 
-const app = express();
+    $(document).on("click", ".more-button", function() {
+        $("#results-container").remove(".more-button-container");
+        ajaxRequest();
+    });
 
-app.use(bodyParser.urlencoded({extended: true}));
+    // Lets users scrolls down the page
+    function scroll_position() {
+        //If it reached to the bottom
+        var reach_buttom = 
+            $(document).scrollTop() + $(window).height() >=
+            $(document).height() - 100;
+        if (reach_buttom) {
+            //When the user reached to the bottom call the ajaxRequest
+            ajaxRequest();
+        } else {
+            //Set a timer for 1 second
+            setTimeout(scroll_position, 1000);
+        }
+    }
 
-var spotifyApi = new SpotifyWebApi({
-    clientId: ,
-    clientSecret:,
-    redirectUri: 'http://localhost:8888/callback '
-});
+    function getNewResults() {
+        users_input = $('input[name="user-input"]').val();
+        categoryList = $(".category").val();
+        $("#results-container").html(music_info);
+        ajaxRequest();
+    }
+    function getNextUrl(res) {
+        spotifyUrl = res.next &&
+            res.next.replace("https://api.spotify.com/v1/search", glitchUrl
+            );
+    }
 
-spotifyApi
-.clientCredentialsGrant()
-.then(data=>{
-    console.log(data.body)
-    //spotifyApi.setAccessToken(data.body["access_token"]);
-}).catch(error => {
-    console.log("Can't retrieve access token", error);
-});
+    function ajaxRequest() {
+        $.ajax({
+            url: spotifyUrl || glitchUrl,
+            data: {
+                query: users_input,
+                type: categoryList
+            },
+            success: function(res) {
+                res = res.artists || res.albums;
+                getResultsHtml(res);
+                getNextUrl(res);
+                if (infiniteScroll == location.search) {
+                    scroll_position();
+                }
+            }
+        });
+    }
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/search.html");
-});
-app.post("/", function(req, res){
-    console.log(req.body.userInput);
-    const query = req.body.userInput;
-    const apiKey = "";
-    const url = "https://api.spotify.com/v1/search"
-})
-app.listen(3000, function(){
-    console.log("Server is running on port 3000")
-})
+    function getResultsHtml(res) {
+        var imageUrl,
+            buttonHtml = "";
+        if (res.items.length == 0) {
+            search_results =
+                "There are no results that match your search. Please try again with another search query.";
+            $("#results-text").text(search_results);
+        } else {
+            search_results =
+                res.total + " results for '" + users_input + "':";
+            $("#results-text").text(search_results);
+            var i = 0;
+            for (i = 0; i < res.items.length; i++) {
+                if (res.items.length >= 20) {
+                    buttonHtml =
+                        '<div class="more-button-container"><button class="more-button">Show more results</button></div>';
+                }
+
+                if (res.items[i].images.length === 0) {
+                    imageUrl =
+                        "https://developer.spotify.com/assets/branding-guidelines/icon2@2x.png";
+                } else {
+                    imageUrl = res.items[i].images[0].url;
+                }
+
+                var artistUrl = res.items[i].external_urls.spotify;
+                var artistAlbumName = res.items[i].name;
+                music_info +=
+                    '<div class="one-of-twenty-container"><div class="results-albums-artists"><a href="' +
+                    artistUrl +
+                    '" target="_blank"><img src="' +
+                    imageUrl +
+                    '"></a></div><div class="results-name"><a href="' +
+                    artistUrl +
+                    '" target="_blank">' +
+                    artistAlbumName +
+                    "</a></div></div>";
+            }
+            $("#results-container").html(music_info);
+            if (i == 20 && !infiniteScroll == location.search) {
+                $("#results-container").append(buttonHtml);
+            }
+        }
+    }
+
+})();
