f97c3ab9c93f2af16645c6cdecd12b9204253d18
drishan <43171376+drish96@users.noreply.github.com>
2020-05-03 01:12:43 -0700

    Add files via upload

--- routes/comment.js
+++ routes/comment.js
@@ -1,14 +1,45 @@
 const express = require('express')
-const router = express.Router()
-const db - require('../db')
+let router = express.Router()
 
-router.get('/', (req,res) => {
-	db.any("SELECT message FROM comment;")
-	.then(rows => {
-		console.log(rows);
-		res.json(rows)
+var pgp = require('pg-promise')();
+
+const dbConfig = {
+	host: 'localhost',
+	port: 5432,
+	database: 'valence_db',
+	user: 'postgres',
+	password: 'pwd'
+};
+
+var db = pgp(dbConfig);
+
+
+router.use(function(req, res, next) {
+	console.log(req.url, "@", Date.now());
+	next();
+});
+
+router
+.route("/public")
+.get((req,res) => {
+	var query = 'SELECT comment_id, message FROM comment;';
+	db.any(query)
+	.then(function (data) => {
+			res.render('public/home', {
+				commenter: data[0]
+				message: data[1]
+			})
+		// res.send(data)
+		// render the comments
 	})
 	.catch(error => {
+		req.flash('error', err);
 		console.log(error)
 	})
 })
+.post((req, res) => {
+    res.send("hi post /public/home");
+    // send to db
+});
+
+module.exports = router;
